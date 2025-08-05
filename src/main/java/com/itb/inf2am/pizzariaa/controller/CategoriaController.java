f458905562fee24a5efa31fe9daff36c72f8b2f7
araujoo77 <elivanlimadearaujo@gmail.com>
2025-08-05 08:34:52 -0300

    Configuracao da conexao com o banco de dados

--- src/main/java/com/itb/inf2am/pizzariaa/controller/CategoriaController.java
+++ src/main/java/com/itb/inf2am/pizzariaa/controller/CategoriaController.java
@@ -1,32 +1,44 @@
 package com.itb.inf2am.pizzariaa.controller;
 
+import com.itb.inf2am.pizzariaa.model.entity.Produto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/categoria")
 public class CategoriaController {
 
-    public Long id;
-    public String nome;
-    public String descricao;
-    public CategoriaController(Long id, String nome, String descricao) {
-        this.id = id;
+    List<Categoria> categorias = new ArrayList<Categoria>();
+
+
+    @GetMapping
+    public List<Categoria> findAll() {
+
+        categoria p1 = new Categoria();
+        p1.setNome("Pizza de Calabreza");
+        p1.setDescricao("Pizza com muita Calabreza e cebola");
+        p1.setValorVenda(49.50);
+
+        Categoria p2 = new Categoria();
+        p2.setNome("Pizza de Calabreza");
+        p2.getDescricao("Pizza com queijo");
+        p2.setValorVenda(49.50);
+
+        // Adicionando o produto
+        categorias.add(p1);
+        categorias.add(p2);
+
+        return categorias;
+
+
     }
-    public CategoriaController() {
-        super();
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        
-    }
+
+
 }
+
